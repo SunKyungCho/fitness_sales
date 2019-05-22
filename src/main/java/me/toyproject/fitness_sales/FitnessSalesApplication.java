2222b377002e662a7dd301ef615e89053be7481b
sunkyung <sunkyungch@gmail.com>
2019-05-22 23:49:20 +0900

    project init

---
+++ src/main/java/me/toyproject/fitness_sales/FitnessSalesApplication.java
@@ -0,0 +1,13 @@
+package me.toyproject.fitness_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessSalesApplication.class, args);
+    }
+
+}
